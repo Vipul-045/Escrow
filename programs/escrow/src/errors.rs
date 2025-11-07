7b1e7f73f63378070cce23b52f94b6fd84551a97
Vipul-045 <vipulyadav0479@gmail.com>
2025-11-07 12:19:51 +0530

    error.rs

---
+++ programs/escrow/src/errors.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum EscrowEroor{
+    #[msg("The escrow has already expired")]EscrowExpired,
+
+    #[msg("The escrow is still active and canoot be cannot canceled yet")]EscrowStillActive,
+
+    #[msg("Escrow is not expired yet")]EscrowNotExpired,
+}
